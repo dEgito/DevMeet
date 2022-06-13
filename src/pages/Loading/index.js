fe6d64862fdbbc85bc0aac68be5ff9a993e4b245
Danielly Egito <egitodany@gmail.com>
2022-06-13 17:14:35 -0300

    fix: remove pasta não utilizada

--- src/pages/Loading/index.js
+++ src/pages/Loading/index.js
@@ -1,11 +1,11 @@
-import {Container, Content, Logo} from "./style";
-import Background from "../../assets/Pattern.svg"
-import LogoIcon from "../../assets/logo.svg"
+import { Container, Content, Logo } from "./style";
+import Background from "../../assets/Pattern.svg";
+import LogoIcon from "../../assets/logo.svg";
 
 function Loading() {
   return (
     <Container>
-      <Content src={Background}/>
+      <Content src={Background} />
       <Logo src={LogoIcon} />
     </Container>
   );
