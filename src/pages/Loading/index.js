9dc9d9339cdabdcdb473ff033197dfbd669c3acc
Danielly Egito <egitodany@gmail.com>
2022-06-13 17:02:36 -0300

    feat: adiciona tela "Loading"

--- src/pages/Loading/index.js
+++ src/pages/Loading/index.js
@@ -1,5 +1,14 @@
-function Loading (){
-    return (<h1>Loading</h1>)
+import {Container, Logo} from "./style";
+import Background from "../../assets/Pattern.svg"
+import LogoIcon from "../../assets/logo.svg"
+
+function Loading() {
+  return (
+    <Container>
+      <img src={Background}/>
+      <Logo src={LogoIcon} />
+    </Container>
+  );
 }
 
 export default Loading;
