b70ff24f5d0615b8419e8d1501bbda8fa47013c0
Danielly Egito <egitodany@gmail.com>
2022-06-13 17:02:36 -0300

    feat: adiciona tela "Loading"

---
+++ src/pages/Loading/style.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const Container = styled.body`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  height: 100vh;
+  background-color: #282828;
+
+  img {
+    position: absolute;
+  }
+`;
+
+export const Logo = styled.img`
+  display: flex;
+  animation: scale 2s infinite;
+
+  @keyframes scale {
+    0% {
+        transform: scale(1);
+    }
+
+    50% {
+        transform: scale(1.1);
+    }
+
+    100% {
+        transform: scale(1);
+    }
+  }
+`;
