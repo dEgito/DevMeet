0e2d5923be01dd2a170fa68b31f04647d29703a6
Danielly Egito <egitodany@gmail.com>
2022-06-21 21:51:16 -0300

    feat: adiciona tela Events

--- src/pages/Loading/style.js
+++ src/pages/Loading/style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.body`
+export const Container = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
